a906d8fd337f2b2f7ca2d9a6b2e5b6ecbe789a0c
syber <devnull@localhost>
2019-06-03 02:15:07 +0300

    UniEvent::WebSocket upgrade

--- plus/UniEvent/src/xs/unievent.h
+++ plus/UniEvent/src/xs/unievent.h
@@ -115,29 +115,36 @@ template <class TYPE> struct Typemap<panda::unievent::Error*, TYPE*> : TypemapOb
         return Super::out(aTHX_ var, sv ? sv : xs::unievent::get_perl_class_for_err(*var));
     }
 };
+template <class TYPE> struct Typemap<panda::unievent::CodeError*, TYPE>  : Typemap<panda::unievent::Error*,     TYPE> {};
+template <class TYPE> struct Typemap<panda::unievent::SSLError*,  TYPE>  : Typemap<panda::unievent::CodeError*, TYPE> {};
 
 template <class TYPE> struct Typemap<const panda::unievent::Error&, TYPE&> : Typemap<panda::unievent::Error*, TYPE*> {
     using Super = Typemap<panda::unievent::Error*, TYPE*>;
 
-    static Sv out (pTHX_ TYPE& var, const Sv& sv = {}) {
-        if (!var) return Sv::undef;
-        return Super::out(aTHX_ var.clone(), sv);
+    static Sv out (pTHX_ TYPE& var, const Sv& sv = {}) { return Super::out(aTHX_ var.clone(), sv); }
+
+    static TYPE& in (pTHX_ const Sv& sv) {
+        auto ret = Super::in(aTHX_ sv);
+        if (!ret) throw "error must not be undef here";
+        return *ret;
     }
+};
+
+template <class TYPE> struct Typemap<const panda::unievent::CodeError&, TYPE&> : Typemap<const panda::unievent::Error&, TYPE&> {
+    using Super = Typemap<const panda::unievent::Error&, TYPE&>;
+
+    static Sv out (pTHX_ TYPE& var, const Sv& sv = {}) { return var ? Super::out(aTHX_ var, sv) : Sv::undef; }
 
     static TYPE& in (pTHX_ const Sv& sv) {
         if (!sv.defined()) {
             static TYPE ret;
             return ret;
         }
-        return *Super::in(aTHX_ sv);
+        return Super::in(aTHX_ sv);
     }
 };
 
-template <class TYPE> struct Typemap<panda::unievent::CodeError*, TYPE>  : Typemap<panda::unievent::Error*,     TYPE> {};
-template <class TYPE> struct Typemap<panda::unievent::SSLError*,  TYPE>  : Typemap<panda::unievent::CodeError*, TYPE> {};
-
-template <class TYPE> struct Typemap<const panda::unievent::CodeError&, TYPE&> : Typemap<const panda::unievent::Error&,     TYPE&> {};
-template <class TYPE> struct Typemap<const panda::unievent::SSLError&,  TYPE&> : Typemap<const panda::unievent::CodeError&, TYPE&> {};
+template <class TYPE> struct Typemap<const panda::unievent::SSLError&, TYPE&> : Typemap<const panda::unievent::CodeError&, TYPE&> {};
 
 template <> struct Typemap<panda::unievent::AddrInfoHints> : TypemapBase<panda::unievent::AddrInfoHints> {
     using Hints = panda::unievent::AddrInfoHints;
