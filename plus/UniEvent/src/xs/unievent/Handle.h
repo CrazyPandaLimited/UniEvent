8729e753546ca146ff0a9bef7637041b2fdf915d
g.smorkalov <g.smorkalov@crazypanda.ru>
2020-08-12 20:52:52 +0300

    UE bugfix

--- plus/UniEvent/src/xs/unievent/Handle.h
+++ plus/UniEvent/src/xs/unievent/Handle.h
@@ -12,6 +12,7 @@ namespace unievent {
 template <class TYPE> struct Typemap<panda::unievent::Handle*, TYPE> : TypemapObject<panda::unievent::Handle*, TYPE, ObjectTypeRefcntPtr, ObjectStorageMGBackref, DynamicCast> {
     using Super = TypemapObject<panda::unievent::Handle*, TYPE, ObjectTypeRefcntPtr, ObjectStorageMGBackref, DynamicCast>;
     static Sv create (const TYPE& var, Sv proto = Sv()) {
+        if (!var) return &PL_sv_undef;
         if (!proto) {
             proto = unievent::handle_perl_class(var->type());
         }
