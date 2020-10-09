686c0b03c50fdcd893f01c96c68bb1975ea9e52e
syber <syber@crazypanda.ru>
2020-10-09 18:07:42 +0300

    libunievent: fix streamer-stream test

--- tests/streamer/stream.cc
+++ tests/streamer/stream.cc
@@ -69,8 +69,8 @@ TEST("pause input") {
 
 TEST("normal output") {
     AsyncTest test(3000, 2);
-    auto p1 = make_pair(test.loop, 10000, 20);
     auto p2 = make_p2p(test.loop);
+    auto p1 = make_pair(test.loop, 10000, 20);
     auto i = new TestStreamInput(p1.sconn);
     auto o = new StreamOutput(p2.sconn);
     StreamerSP s = new Streamer(i, o, 50000, test.loop);
@@ -83,7 +83,7 @@ TEST("normal output") {
     });
 
     string res;
-    p2.client->read_event.add([&](auto&, string& data, auto...) {
+    p2.client->read_event.add([&](auto&, const string& data, auto...) {
         res += data;
     });
     p2.client->eof_event.add([&](auto...){
